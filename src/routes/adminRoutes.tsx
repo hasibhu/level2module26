b5c3484e0e2b86e00a29bdd46009e443dde5ebbf
hasibhu <hasibul1987@gmail.com>
2025-01-11 17:03:17 +0100

    admin routes have been refactored

--- src/routes/adminRoutes.tsx
+++ src/routes/adminRoutes.tsx
@@ -4,7 +4,40 @@ import AdminDashboard from "../pages/admin/AdminDashboard";
 import CreateStudent from "../pages/admin/CreateStudent";
 import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateAdmin from "../pages/admin/CreateAdmin";
+import { Children } from "react";
 
+
+
+const adminRoutes2 = [
+    {
+        name: 'Dashboard',
+        path: '/admin/dashboard',
+        element: <AdminDashboard></AdminDashboard>
+    },
+    {
+        name: 'User Management',
+        Children: [
+            {
+                name: 'Create Student',
+                path: '/admin/createStudent',
+                element: <CreateStudent />,
+                
+            },
+            {
+                name: 'Create Faculty',
+                path: '/admin/createFaculty',
+                element: <CreateFaculty />,
+                
+            },
+            {
+                name: 'Create Admin',
+                path: '/admin/createAdmin',
+                element: <CreateAdmin />,
+                
+            },
+        ]
+    }
+]
 
 
 export const adminRoutes = [
